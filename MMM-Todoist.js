1f200c615b40be717c275bd79b15c6928f3a23b3
AgP42 <agp.com@mailbox.org>
2018-07-18 19:13:44 +0200

    Improvment
    Modification added : 
    
    - Management of a PIR sensor with the module MMM-PIR-Sensor (by PaViRo). In case PIR module detect no user, the update of the ToDoIst is stopped and will be requested again at the return of the user
    
    - Management of the "module.hidden" by the core system : same behaviour as "User_Presence" by the PIR sensor
    
    - Add "Loading..." display when the infos are not yet loaded from the server
    
    - Possibility to add the last update time from server at the end of the module. This can be configured using "displayLastUpdate" and "displayLastUpdateFormat"
    
    - Possibility to display long task on several lines(using the code from default module "calendar". This can be configured using "wrapEvents" and "maxTitleLength"

--- MMM-Todoist.js
+++ MMM-Todoist.js
@@ -7,6 +7,23 @@
  *
  * MIT Licensed.
  */
+ 
+ 
+ /*
+  * Update by AgP42 the 18/07/2018
+  * Modification added : 
+  * - Management of a PIR sensor with the module MMM-PIR-Sensor (by PaViRo). In case PIR module detect no user, 
+  * the update of the ToDoIst is stopped and will be requested again at the return of the user
+  * - Management of the "module.hidden" by the core system : same behaviour as "User_Presence" by the PIR sensor
+  * - Add "Loading..." display when the infos are not yet loaded from the server
+  * - Possibility to add the last update time from server at the end of the module. 
+  * This can be configured using "displayLastUpdate" and "displayLastUpdateFormat"
+  * - Possibility to display long task on several lines(using the code from default module "calendar".
+  * This can be configured using "wrapEvents" and "maxTitleLength"
+  * */
+ 
+//UserPresence Management (PIR sensor)
+var UserPresence = true; //true by default, so no impact for user without a PIR sensor
 
 Module.register("MMM-Todoist", {
 
@@ -17,6 +34,13 @@ Module.register("MMM-Todoist", {
 		fade: true,
 		fadePoint: 0.25,
 		sortType: "todoist",
+		
+		//New config from AgP42
+		displayLastUpdate: false, //add or not a line after the tasks with the last server update time
+		displayLastUpdateFormat: 'dd - HH:mm:ss', //format to display the last update. See Moment.js documentation for all display possibilities
+		maxTitleLength: 25, //10 to 50. Value to cut the line if wrapEvents: true
+		wrapEvents: false, // wrap events to multiple lines breaking at maxTitleLength
+		
 		showProject: true,
 		projectColors: ["#95ef63", "#ff8581", "#ffc471", "#f9ec75", "#a8c8e4", "#d2b8a3", "#e2a8e4", "#cccccc", "#fb886e",
 			"#ffcc00", "#74e8d3", "#3bd5fb", "#dc4fad", "#ac193d", "#d24726", "#82ba00", "#03b3b2", "#008299",
@@ -44,6 +68,14 @@ Module.register("MMM-Todoist", {
 	start: function() {
 		var self = this;
 		Log.info("Starting module: " + this.name);
+		
+		var updateIntervalID = 0; // Definition of the IntervalID to be able to stop and start it again
+		var ModuleToDoIstHidden = false; // by default it is considered displayed. Note : core function "this.hidden" has strange behaviour, so not used here
+		var lastUpdate = 0; //timestamp of the last module update. set at 0 at start-up
+		
+		//to display "Loading..." at start-up
+		this.title = "Loading...";
+		this.loaded = false;
 
 		if (this.config.accessToken === "") {
 			Log.error("MMM-Todoist: AccessToken not set!");
@@ -57,18 +89,126 @@ Module.register("MMM-Todoist", {
 			}
 		}
 
-		this.sendSocketNotification("FETCH_TODOIST", this.config);
+//		this.sendSocketNotification("FETCH_TODOIST", this.config); //removed from start-up as it is now requested by "resume" core function just after start-up. Removed here to avoid double request to the server. impact : 2~3seconds of delay at start-up
 
-		setInterval(function () {
+		//add ID to the setInterval functionto be able to stop it later on
+		this.updateIntervalID = setInterval(function () {
 			self.sendSocketNotification("FETCH_TODOIST", self.config);
 		}, this.config.updateInterval);
+		
+//		Log.log(this.name + " a fini de démarrer, projects - updateIntervalID : " + this.config.projects + " - " + this.updateIntervalID);
+		
 	},
 
+//Modif AgP42 - 16/07/2018	
+
+	suspend: function() { //called by core system when the module is not displayed anymore on the screen
+		this.ModuleToDoIstHidden = true; 
+		//Log.log("Fct suspend - ModuleHidden = " + ModuleHidden);
+		this.GestionUpdateIntervalToDoIst();
+	},
+	
+	resume: function() { //called by core system when the module is displayed on the screen
+		this.ModuleToDoIstHidden = false;
+		//Log.log("Fct resume - ModuleHidden = " + ModuleHidden);
+		this.GestionUpdateIntervalToDoIst();	
+	},
+
+	notificationReceived: function(notification, payload) {
+		if (notification === "USER_PRESENCE") { // notification sended by module MMM-PIR-Sensor. See its doc
+			//Log.log("Fct notificationReceived USER_PRESENCE - payload = " + payload);
+			UserPresence = payload;
+			this.GestionUpdateIntervalToDoIst();
+		}
+	},
+
+	GestionUpdateIntervalToDoIst: function() {
+		if (UserPresence === true && this.ModuleToDoIstHidden === false){ 
+			var self = this;
+		//	Log.log(this.name + " est revenu et user present ! On update : " + this.config.projects);
+	
+			// update now
+			this.sendSocketNotification("FETCH_TODOIST", this.config);
+		//	Log.log("Update immédiat demandé pour : " + this.config.projects);
+
+			
+			//if no IntervalID defined, we set one again. This is to avoid several setInterval simultaneously
+			if (this.updateIntervalID === 0){
+						
+				this.updateIntervalID = setInterval(function () {
+					self.sendSocketNotification("FETCH_TODOIST", self.config);
+					}, this.config.updateInterval);
+					
+		//	Log.log("Remise en route timer, projects - updateIntervalID : " + this.config.projects + " - " + this.updateIntervalID);
+
+			}
+			
+		}else{ //if (UserPresence = false OR ModuleHidden = true)
+			Log.log("Personne regarde : on stop l'update " + this.name + " projet : " + this.config.projects);
+			clearInterval(this.updateIntervalID); // stop the update interval of this module
+			this.updateIntervalID=0; //reset the flag to be able to start another one at resume
+		}
+	},
+	
+	
+//Code from MichMich from default module Calendar : to manage task displayed on several lines
+
+	/**
+	 * Shortens a string if it's longer than maxLength and add a ellipsis to the end
+	 *
+	 * @param {string} string Text string to shorten
+	 * @param {number} maxLength The max length of the string
+	 * @param {boolean} wrapEvents Wrap the text after the line has reached maxLength
+	 * @returns {string} The shortened string
+	 */
+	shorten: function (string, maxLength, wrapEvents) {
+		if (typeof string !== "string") {
+			return "";
+		}
+
+		if (wrapEvents === true) {
+			var temp = "";
+			var currentLine = "";
+			var words = string.split(" ");
+
+			for (var i = 0; i < words.length; i++) {
+				var word = words[i];
+				if (currentLine.length + word.length < (typeof maxLength === "number" ? maxLength : 25) - 1) { // max - 1 to account for a space
+					currentLine += (word + " ");
+				} else {
+					if (currentLine.length > 0) {
+						temp += (currentLine + "<br>" + word + " ");
+					} else {
+						temp += (word + "<br>");
+					}
+					currentLine = "";
+				}
+			}
+
+			return (temp + currentLine).trim();
+		} else {
+			if (maxLength && typeof maxLength === "number" && string.length > maxLength) {
+				return string.trim().slice(0, maxLength) + "&hellip;";
+			} else {
+				return string.trim();
+			}
+		}
+	},
+//end modif AgP
 
 	// Override socket notification handler.
 	socketNotificationReceived: function(notification, payload) {
 		if (notification === "TASKS") {
 			this.filterTodoistData(payload);
+			
+			if(this.config.displayLastUpdate){
+				this.lastUpdate = Date.now() / 1000 ; //save the timestamp of the last update to be able to display it		
+			}
+			
+			Log.log("ToDoIst update OK, projet : " + this.config.projects + " at : " 
+			+ moment.unix(this.lastUpdate).format(this.config.displayLastUpdateFormat)); //AgP
+			
+			this.loaded = true;
 			this.updateDom(1000);
 		}else if (notification === "FETCH_ERROR") {
 			Log.error("Todoist Error. Could not fetch todos: " + payload.error);
@@ -152,9 +292,19 @@ Module.register("MMM-Todoist", {
 
 
 	getDom: function() {
+		
+		//Add a new div to be able to display the update time alone after all the task
+		var wrapper = document.createElement("div");
+		
+		//display "loading..." is not loaded
+		if (!this.loaded) {
+			wrapper.innerHTML = "Loading...";
+			wrapper.className = "dimmed light small";
+			return wrapper;
+		}
+		
 		var table = document.createElement("table");
 		table.className = "normal small light";
-
 
 		if (this.tasks === undefined) {
 			return table;
@@ -186,7 +336,8 @@ Module.register("MMM-Todoist", {
 
 			var todoCell = document.createElement("td");
 			todoCell.className = "title bright alignLeft";
-			todoCell.innerHTML = this.tasks.items[i].content;
+//			todoCell.innerHTML = this.tasks.items[i].content;
+			todoCell.innerHTML = this.shorten(this.tasks.items[i].content, this.config.maxTitleLength, this.config.wrapEvents);
 			row.appendChild(todoCell);
 
 			var dueDateCell = document.createElement("td");
@@ -268,6 +419,7 @@ Module.register("MMM-Todoist", {
 				projectCell.innerHTML = project.name + "<span class='projectcolor' style='color: " + projectcolor + "; background-color: " + projectcolor + "'></span>";
 				row.appendChild(projectCell);
 			}
+			
 
 			// Create fade effect by MichMich (MIT)
 			if (this.config.fade && this.config.fadePoint < 1) {
@@ -283,8 +435,20 @@ Module.register("MMM-Todoist", {
 			}
 			// End Create fade effect by MichMich (MIT)
 		}
+		wrapper.appendChild(table); //quand la table est finie (loop des sensors finie), on l'ajoute au wrapper
 
-		return table;
+		
+		// display the update time at the end, if defined so by the user config
+		if(this.config.displayLastUpdate){
+
+			var updateinfo = document.createElement("div");
+			updateinfo.className = "xsmall light align-left";
+			updateinfo.innerHTML = "Update : " + moment.unix(this.lastUpdate).format(this.config.displayLastUpdateFormat);
+			wrapper.appendChild(updateinfo);
+		}
+
+		return wrapper;
 	}
+	
 
 });
